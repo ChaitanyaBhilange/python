c77f5cbb053a6f0e96c15011eec3a854baca6f47
ChaitanyaBhilange <107292155+ChaitanyaBhilange@users.noreply.github.com>
2022-06-10 22:32:56 -0700

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,21 @@
+#to check python keyword
+import keyword
+print(keyword.kwlist)
+#add,sub,muit,div
+#addition
+a=5 
+b=6
+add=a+b
+print('addition=',add)
+#substraction
+sub=a-b
+print(sub)
+#multipaction
+a=5
+b=6
+mult=a*b
+print(mult)
+#division
+div=a/b
+print(div)
+
